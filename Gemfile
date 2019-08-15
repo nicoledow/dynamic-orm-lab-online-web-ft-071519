426fec25c1e764055b28b609e18aaa5f4540194e
Nicole Dow <nikki.dow71@gmail.com>
2019-08-15 23:47:20 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source "https://rubygems.org"
 
 gem 'pry'
 gem 'sqlite3', '~>1.3.6'
+gem 'activesupport-inflector', '~> 0.1.0'
--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,13 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  attr_accessor :name, :grade
+  attr_reader :id
+  
+  def initialize(name: name, grade: grade, id: nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
 end
